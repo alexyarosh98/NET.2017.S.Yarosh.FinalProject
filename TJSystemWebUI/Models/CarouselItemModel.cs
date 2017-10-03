906eb8cdae56e49353d29d687af22677e59e96b1
Alexander <alexyarosh98@gmail.com>
2017-10-03 23:54:51 +0300

    carousel 2.0 + lightbox 1.0

---
+++ TJSystemWebUI/Models/CarouselItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TJSystemWebUI.Models
+{
+    public class CarouselItemModel
+    {
+        public string  ImagePath { get; set; }
+        public string ImageInfo { get; set; }
+    }
+}
